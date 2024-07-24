9be180befbc09f9f5f95909b9ac8acade2d53ec9
Eugene Che <indexpro24@gmail.com>
2024-07-24 14:52:58 +0300

    c

--- project/src/UI/index.js
+++ project/src/UI/index.js
@@ -1,15 +1,27 @@
-export { default as Banner } from './Banner/v1'
-export { default as Footer } from './Footer/v1'
-export { default as Menu } from './Menu/v1'
-export { default as Info } from './Info/v2'
-export { default as Slider } from './Slider/v1'
+// Layout
+export { default as Banner } from './Banner'
+export { default as Footer } from './Footer'
+export { default as Menu } from './Menu'
+
+// About
+export { default as About } from './About'
+
+// Contacts
+export { default as Address } from './Address'
+export { default as Map } from './Map'
+
+
+// Catalog
 export { default as Rubric } from './Rubric/v1'
-export { default as About } from './About/v1'
-export { default as Category } from './Category/v1'
 export { default as ProductList } from './ProductList/v1'
 export { default as Product } from './Product/v2'
-export { default as Address } from './Address/v1'
-export { default as Map } from './Map/v1'
+export { default as Category } from './Category/v1'
+
+
+
+// ----
+export { default as Info } from './Info/v2'
+export { default as Slider } from './Slider/v1'
 export { default as Testimonials } from './Testimonials/v1'
 export { default as Service } from './Service/v1'
 export { default as Cart } from './Cart/v1'
